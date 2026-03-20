959d840929aafa3fec6800b64bf774f3ebb48247
Narf <vidal.carregal.francisco@gmail.com>
2026-03-20 09:39:20 +0100

    removed debouncedCallback dependancy

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname, useRouter} from 'next/navigation';
-import { useDebouncedCallback} from 'use-debounce';
+//import { useDebouncedCallback} from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-    const handleSearch = useDebouncedCallback((term) => {
+    //const handleSearch = useDebouncedCallback((term) => {
+    function handleSearch(term:string){
       console.log(term);
       const params = new URLSearchParams(searchParams);
 
@@ -21,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       }
       replace(`${pathname}?${params.toString()}`);
 
-    },300);
+    }//,300);
 
 
   return (
